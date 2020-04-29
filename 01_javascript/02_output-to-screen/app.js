832335347408c1c06c2eabebbca4f96e464b7535
Christian Tisby <cduane82@hotmail.com>
2020-04-29 03:59:24 -0400

    adding variables

---
+++ 01_javascript/02_output-to-screen/app.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML = "I Like Girls"
+
+document.write("I really like women ladies")
+
+window.alert("Yes I still really like girls")
+
+console.log("I aint gonna lie, I like women alot lot lot")
---
+++ 01_javascript/04_variables/app.js
@@ -0,0 +1,7 @@
+var firstName = "Christian"
+var lastName = "Tisby"
+var name = firstName + " " + lastName
+var age = 37
+var city = "Tallahassee"
+var state = "Florida"
+console.log(name)
